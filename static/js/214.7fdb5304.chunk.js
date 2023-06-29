e5ae8bae5ebc1913f968dc028ed288fe2e9df73d
stanislav-kintoriak <114350380+stanislav-kintoriak@users.noreply.github.com>
2023-06-29 07:15:23 +0000

    Deploying to gh-pages from @ stanislav-kintoriak/goit-react-hw-08-phonebook@4266dddf6a7117ad698e4f6147d79d313caab389 🚀

---
+++ static/js/214.7fdb5304.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkreact_homework_template=self.webpackChunkreact_homework_template||[]).push([[214],{3799:function(e,a,t){t.r(a),t.d(a,{default:function(){return o}});var n=t(8634),c=t.p+"static/media/funny-phone-book.6d009f4c3c690b8d3a46.jpg",r=t(184),o=function(){return(0,r.jsx)(r.Fragment,{children:(0,r.jsx)(n.Z,{children:(0,r.jsx)("img",{src:c,alt:"phonebookImage",className:"css.homepage_image"})})})}}}]);
+//# sourceMappingURL=214.7fdb5304.chunk.js.map
